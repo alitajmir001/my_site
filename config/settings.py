634fdd4a697542649cdcce584947829fed9acf90
alitajmir001 <alitajmir001@gmail.com>
2026-09-20 14:17:33 +0330

    Configure Cloudinary storage for media and static files
    Updated file storage settings to use Cloudinary for both media and static files.

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,20 @@ INSTALLED_APPS = [
     'cloudinary',
     
 ]
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "cloudinary_storage.storage.StaticHashedCloudinaryStorage",
+    },
+}
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
